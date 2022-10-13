826f0ca2a43c3df6a77e1038c8528a19cce7f606
William Santos <110426773+willsantos86@users.noreply.github.com>
2022-10-13 18:37:56 -0300

    Atualização  do projeto

--- TODO.py
+++ TODO.py
@@ -16,29 +16,15 @@ import sqlite3
 conexao = sqlite3.connect('exercicio_TODO')
 cursor = conexao.cursor()
 #cursor.execute('CREATE TABLE categorias (id_categorias INTEGER PRIMARY KEY AUTOINCREMENT, nome_categoria VARCHAR(100));')
-opcao = int(input('1- EXCLUIR TABELA\n2- ATUALIZAR O NOME DA TABELA\n3- CRIAR TABELA TAREFAS\n4- INSERIR COLUNAS NA TABELA CATEGORIAS\n5- INSERIR TAREFAS\n6- COLUNA'))
+#cursor.execute('CREATE TABLE tarefas(id_tarefas INTEGER PRIMARY KEY AUTOINCREMENT, nome_tarefa VARCHAR(100), status_tarefa VARCHAR(100), id_categorias INT NOT NULL, FOREIGN KEY(id_categorias) REFERENCES categorias(id_tarefas));')
+#cursor.execute('ALTER TABLE categorias RENAME TO categorias_TODO')
+
+opcao = int(input('1- INSERIR VALORES NAS TABELAS\n2- INSERIR COLUNAS NA TABELA CATEGORIAS\n3- INSERIR TAREFAS\n6- COLUNA'))
 print('-='*15)
+    
 if opcao == 1:
-    exclusao = int(input('QUAL TABELA DESEJA EXCLUIR?\n1- CATEGORIAS\n2- TAREFAS'))
-    if exclusao == 1:
-        cursor.execute('DROP TABLE categorias_TODO')
-    else:
-        cursor.execute('DROP TABLE tarefas')
-
-elif opcao == 2:
-    cursor.execute('ALTER TABLE categorias RENAME TO categorias_TODO')
-
-elif opcao == 3:
-    colunas1 = []
-    nome_tabela = input('DIGITE O NOME DA TABELA QUE VOCÊ DESEJA CRIAR:\n')
-    quant_colunas = int(input(f'DIGITE A QUANTIDADE DE COLUNAS QUE DESEJA CRIAR NA TABELA\n'))
-    for coluna in range(quant_colunas):
-        colunas1.append(input(f'Digite o nome da {coluna+1}º coluna'))
-    colunas= [nome_tabela, colunas1]
-    inseri = "CREATE TABLE (?)((?) INTEGER PRIMARY KEY AUTOINCREMENT,(?) VARCHAR(100));"
-    cursor.execute(inseri, colunas)
-
-elif opcao == 4:
+    tabela = int(input('Qual Tabela deseja inserir valores'))
+    if 
     nome_categoria = str(input('Digite a categoria:\n'))
     valores = [nome_categoria]
     inserir = 'INSERT INTO categorias(nome_categoria) VALUES(?)'
@@ -46,16 +32,17 @@ elif opcao == 4:
 
     #cursor.execute('INSERT INTO categorias(nome_categoria) VALUES("Trabalho"), ("Familia"), ("Faculdade"), ("Casa");')
 
-elif opcao == 5:
+elif opcao == 2:
     nome_tarefa = str(input('Digite a tarefa:\n'))
     status = str(input('Digite o status da tarefa:\n'))
-    categoria_id = str(input('Digite o ID da categoria:\nTRABALHO = 1\nFAMILIA = 2\nFACULDADE = 3\nCASA = 4\n'))
+    categoria_id = str(input('Digite o ID da categoria:\nTRABALHO = 1\nFAMILIA = 2\nFACULDADE = 3\nCASA = 4\nIGREJA = 5\nVIAJEM = 6'))
     valores = [nome_tarefa, status, categoria_id]
     inserir = 'INSERT INTO tarefas(nome_tarefa, status_tarefa, id_categorias) VALUES(?, ?, ?)'
     cursor.execute(inserir, valores)
 
 elif opcao == 6:
     escolha_tabela = int(input('QUAL TABELA DESEJA ATUALIZAR?\nCATEGORIAS = 1\nTAREFAS = 2\n'))
+
     if escolha_tabela == 1:
         nome_cat_atua = input('Digite nome atualizado:\n')
         id = int(input('Digite o ID referente ao valor a ser atualizado:\n'))
@@ -65,6 +52,7 @@ elif opcao == 6:
 
     if escolha_tabela == 2:
         coluna = int(input('Qual coluna quer atualizar?\nNOME = 1\nSTATUS = 2\nID CATEGORIA = 3\n'))
+
         if coluna == 1:
             id_tarefa = int(input('Digite o ID referente ao valor a ser atualizado:\n'))
             nome_tarefa_atualizado = input('Digite nome atualizado:\n')
@@ -79,13 +67,19 @@ elif opcao == 6:
             atualizacao = 'UPDATE tarefas SET status_tarefa = (?) where id_tarefas= (?)' 
             cursor.execute(atualizacao, valores)
 
-        else:
+        elif coluna == 3:
             id_status = int(input('Digite o ID referente ao status que será atualizado:\n'))
             id_cat_atua = int(input('Digite o novo ID_CATEGORIA atualizado:\n'))
             valores = [id_cat_atua, id_status]
             atualizacao = 'UPDATE tarefas SET id_categorias = (?) where id_tarefas= (?)' 
             cursor.execute(atualizacao, valores)
 
+    elif opcao == 1:
+        exclusao = int(input('QUAL TABELA DESEJA EXCLUIR?\n1- CATEGORIAS\n2- TAREFAS'))
+        if exclusao == 1:
+            cursor.execute('DROP TABLE categorias')
+        else:
+            cursor.execute('DROP TABLE tarefas')
 
 
 conexao.commit()
